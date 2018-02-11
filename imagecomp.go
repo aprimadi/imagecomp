0274ea06c921eaacc3996df3a0a32270d3516c9c
Armin Primadi <aprimadi@gmail.com>
2018-02-11 22:01:53 +0700

    Adding compression size

--- imagecomp.go
+++ imagecomp.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+  "bytes"
   "fmt"
   "image/png"
   "image/jpeg"
+  "io"
+  "io/ioutil"
   "os"
   "path/filepath"
   "regexp"
@@ -14,44 +17,94 @@ import (
 )
 
 func optimizePNG(path string) bool {
+  // Read image
   finput, err := os.Open(path)
   if err != nil {
     panic(err)
   }
-  img, err := png.Decode(finput)
+  input, err := ioutil.ReadAll(finput)
+  if err != nil {
+    panic(err)
+  }
+  in := bytes.NewReader(input)
+  img, err := png.Decode(in)
   finput.Close()
   if err != nil {
     return false
   }
+
+  // Encode image
+  out := new(bytes.Buffer)
   cimg, err := pngquant.Compress(img, "1")
   if err != nil {
     panic(err)
   }
-  f, err := os.Create(path)
-  defer f.Close()
-  err = png.Encode(f, cimg)
+  err = png.Encode(out, cimg)
   if err != nil {
     panic(err)
+  }
+
+  outlen := int64(out.Len())
+  if outlen < in.Size() {
+    // Write to file
+    f, err := os.Create(path)
+    if err != nil {
+      panic(err)
+    }
+    io.Copy(f, out)
+    f.Close()
+
+    saved := (in.Size() - outlen) * 100 / in.Size()
+    fmt.Println(fmt.Sprintf("%02d%% %s", saved, path))
+  } else {
+    fmt.Println(fmt.Sprintf("--- %s", path))
   }
   return true
 }
 
 func optimizeJPEG(path string) bool {
+  // Read image
   finput, err := os.Open(path)
   if err != nil {
     panic(err)
   }
-  img, err := jpeg.Decode(finput)
+  input, err := ioutil.ReadAll(finput)
+  if err != nil {
+    panic(err)
+  }
+  in := bytes.NewReader(input)
+  img, err := jpeg.Decode(in)
   finput.Close()
   if err != nil {
     return false
   }
-  f, err := os.Create(path)
-  defer f.Close()
-  err = mozjpegbin.Encode(f, img, nil)
+
+  // Encode image
+  out := new(bytes.Buffer)
+  err = mozjpegbin.Encode(out, img, &mozjpegbin.Options{
+    Quality: 80,
+    Optimize: true,
+  })
   if err != nil {
     panic(err)
   }
+
+  outlen := int64(out.Len())
+  if outlen < in.Size() {
+    // Write to file
+    f, err := os.Create(path)
+    if err != nil {
+      panic(err)
+    }
+    io.Copy(f, out)
+    f.Close()
+
+    saved := (in.Size() - outlen) * 100 / in.Size()
+    fmt.Println(fmt.Sprintf("%02d%% %s", saved, path))
+  } else {
+    fmt.Println(fmt.Sprintf("--- %s", path))
+  }
+
   return true
 }
 
@@ -66,7 +119,7 @@ func main() {
       panic(err)
     }
     if re.Find([]byte(path)) != nil {
-      fmt.Println(fmt.Sprintf("Skipping %s", path))
+      // fmt.Println(fmt.Sprintf("Skipping %s", path))
       return nil
     }
 
@@ -80,9 +133,7 @@ func main() {
         success = optimizeJPEG(path)
       }
 
-      if success {
-        fmt.Println(fmt.Sprintf("Processing %s", path))
-      } else {
+      if !success {
         fmt.Println(fmt.Sprintf("Skipping %s: Unknown file format", path))
       }
     }
